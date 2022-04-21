a5d8138dcb3023ddc6183618c296e11ee5d67873
Nikol Prokopova <n.prokopova96@gmail.com>
2022-04-21 13:06:22 +0700

    Prevent text overflow in display

--- components/display.js
+++ components/display.js
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 const Display = (props) => {
     return (
         <DisplayWrapper>
-            <DisplayAll>
+            <DisplayAll displayAll = {props.displayAll}>
                 {props.displayAll}
             </DisplayAll>
-            <DisplayCurrent>{props.display}</DisplayCurrent>
+            <DisplayCurrent display = {props.display}>{props.display}</DisplayCurrent>
         </DisplayWrapper>
     );
 };
@@ -19,23 +19,23 @@ const DisplayWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-end;
-  height: 90px;
+  height: 70px;
   padding-bottom: 10px;
   border-radius: 9px;
   margin-bottom: 10px;
 `
 const DisplayAll = styled.p`
   height: 30px;
-  margin: 0px;
-  margin-right: 20px;
-  font-size: 15px;
+  margin: 10px;
+  word-break: break-all;
+  font-size: ${props=>(15-(props.displayAll.length/8))/16}rem;
   padding: 0px;
   color: white;
 `
 const DisplayCurrent = styled.p`
-  margin: 0px;
-  margin-right: 20px;
-  font-size: 2.5vw;
+  margin: 10px;
+  word-break: break-all;
+  font-size:  ${props=>(20-(props.display.length/5))/16}rem;
   padding: 0px;
 `
 export default Display;
